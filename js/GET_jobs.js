0dcbfda0cd7ea9a3c32d6133329254ac84f8d114
endeljobb <endeljobb@gmail.com>
2024-09-29 12:24:00 +0200

    ändrade från annonser till annonsers....

--- js/GET_jobs.js
+++ js/GET_jobs.js
@@ -1,4 +1,4 @@
-app.get('/api/annonser', (req, res) => {
+app.get('/api/annonsers', (req, res) => {
     Ad.find()
       .then(ads => res.status(200).json(ads))
       .catch(err => res.status(500).send(err));
